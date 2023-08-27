baf9655073bfffe6560b4188ccc9e152e3d60e48
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-08-27 13:39:18 +0000

    second attempt

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,11 @@ import React , {useState} from "react";
 const App = () =>{
     const [input, setInput] = useState('');
     return (
-        <div>
+        <div className="app">
             <div className="input">
                 <textarea onChange={(e) => setInput(e.target.value)}></textarea>
             </div>
-            <div className="output">
+            <div className="preview">
                 <h1>{input}</h1>
             </div>
         </div>
